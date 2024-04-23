4bccdb3fb55e610da81b7955f04d9803a29d7cf5
HassanMSh <hassan.m.shamseddine@gmail.com>
2024-04-23 15:40:11 +0300

    fix: made brew script noneinteractive

--- install_brew.sh
+++ install_brew.sh
@@ -2,7 +2,7 @@
 
 # install homebrew
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Add homebrew to path
 ## untested
