0c70a3a52c51de1730abccd29bb38c0d28fa08be
HassanMSh <hassan.m.shamseddine@gmail.com>
2024-04-23 15:37:40 +0300

    fix: removed -y, added some cleaning script

--- setup_minikube.sh
+++ setup_minikube.sh
@@ -10,15 +10,19 @@ minikube_install() {
 
     if curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 | tee -a minikube-download.log; then
         echo "Succesfully downloaded minikube"
+        rm minikube-download.log minikube-linux-amd64
     else
         echo "Error downloading minikube. Check minikube-download.log for details."
+        rm minikube-linux-amd64 
         exit 1
     fi
 
-    if sudo install minikube-linux-amd64 /usr/local/bin/minikube -y | tee -a minikube-install.log; then
+    if sudo install minikube-linux-amd64 /usr/local/bin/minikube | tee -a minikube-install.log; then
         echo "Succesfully installed minikube"
+        rm minikube-install.log minikube-linux-amd64
     else
         echo "Error installed minikube. Check minikube-install.log for details."
+        rm minikube-linux-amd64
         exit 1
     fi
 }
